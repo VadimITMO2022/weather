6085697bd2bf3ecb8e4f53843869e483e14cddf0
Vadim Varshavskiy <vadim_varshavskiy@yahoo.com>
2025-12-26 15:25:30 +0300

    .gitignore

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from callbacks import register_callbacks
 import dash_bootstrap_components as dbc
 
 app = Dash(external_stylesheets=[dbc.themes.DARKLY])
-app.title = "Погодный дашборд"
+app.title = "Погодный дашборд Hello"
 server = app.server
 
 app.layout = create_layout()
